eca8beb9e535c0d8a9862a7325701eb78f1a26f7
Tubagus Adhitya Permana <aditt7116@gmail.com>
2022-07-03 00:53:36 +0700

    update SpecificLettersView

--- LexPlay/Views/CreateUserView.swift
+++ LexPlay/Views/CreateUserView.swift
@@ -8,35 +8,105 @@
 import SwiftUI
 
 struct CreateUserView: View {
-    var body: some View {
-      ZStack {
-        RoundedRectangle(cornerRadius: 25, style: .continuous)
-          .fill(.white)
-          .shadow(radius: 10)
+  
+  @State var test = ""
+  @State var isGoToSpecificLetterView = false
+  
+  var body: some View {
+    ZStack {
+      LinearGradient(gradient: Gradient(colors: [.white, Color("background")]), startPoint: .top, endPoint: .bottom)
+      VStack {
         
-        HStack {
-          Image("lex")
-            .resizable()
-            .frame(width: 99, height: 151)
-            .padding(.trailing, 25)
-            
+        Text("Pilih avatarmu!")
+          .font(.custom(FontStyle.lexendMedium, size: 22))
+        
+        CardAvatar(imageName: "lex", imageWidth: 99, imageHeight: 151, avatarName: "Lex").padding(.bottom, 11)
+        
+        CardAvatar(imageName: "play", imageWidth: 104, imageHeight: 115, avatarName: "Play")
+          .padding(.bottom, 24)
           
-          VStack(alignment: .leading) {
-            Text("I'm")
-              .font(.custom(FontStyle.lexendMedium, size: 21))
-            Text("Lex")
-              .font(.custom(FontStyle.lexendSemiBold, size: 48))
-          }
-        }
         
+        TextField(text: $test) {
+          Text("Username")
+            .font(.custom(FontStyle.lexendMedium, size: 21))
+        }
+        .frame(maxWidth: .infinity, maxHeight: 75)
+        .background(.white)
+        .cornerRadius(38)
+        .multilineTextAlignment(.center)
+        .padding(.bottom, 11)
+        
+        Button {
+          isGoToSpecificLetterView.toggle()
+        } label: {
+          Text("Create Profile")
+            .frame(maxWidth: .infinity, maxHeight: 75)
+            .foregroundColor(.white)
+            .font(.custom(FontStyle.lexendMedium, size: 21))
+          
+        }
+        .background(Color.red)
+        .cornerRadius(38)
         
       }
-      .frame(width: UIScreen.screenWidth, height: 166)
+      .padding([.horizontal], 20)
     }
+    .navigationBarHidden(true)
+    .ignoresSafeArea()
+    
+    
+    // Navigation
+    NavigationLink(isActive: $isGoToSpecificLetterView, destination: {
+      SpecificLettersView()
+    }, label: {
+      EmptyView()
+    })
+    
+  }
+}
+
+struct CardAvatar: View {
+  
+  let imageName: String
+  let imageWidth: CGFloat
+  let imageHeight: CGFloat
+  let avatarName: String
+  
+  var body: some View {
+    ZStack {
+      RoundedRectangle(cornerRadius: 25, style: .continuous)
+        .stroke(.red, lineWidth: 8)
+      RoundedRectangle(cornerRadius: 25, style: .continuous)
+        .fill(.white)
+       
+        
+      
+      HStack {
+        Image(imageName)
+          .resizable()
+          .frame(width: imageWidth, height: imageHeight)
+          .padding(.trailing, 25)
+          .offset(y: 7.5)
+        
+        VStack(alignment: .leading) {
+          Text("I'm")
+            .font(.custom(FontStyle.lexendMedium, size: 21))
+          Text(avatarName)
+            .font(.custom(FontStyle.lexendSemiBold, size: 48))
+        }
+      }
+      
+      
+      
+    }
+    .frame(maxWidth: .infinity, maxHeight: 166)
+    
+    
+  }
 }
 
 struct CreateUserView_Previews: PreviewProvider {
-    static var previews: some View {
-        CreateUserView()
-    }
+  static var previews: some View {
+    CreateUserView()
+  }
 }
