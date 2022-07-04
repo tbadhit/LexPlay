bb2e87b4318668ec3f9a9a5831c6cbe19ece7d2a
Fahmi Al <mfahmiak@gmail.com>
2022-07-04 16:56:30 +0700

    Adjust alphabets raw value lettercase

--- LexPlay/Repositories/Seeder.swift
+++ LexPlay/Repositories/Seeder.swift
@@ -227,264 +227,264 @@ extension Seeder {
     private func initUppercaseAlphabet(context: NSManagedObjectContext) {
         let a = AlphabetEntity(context: context)
         a.uuid = UUID()
-        a.char = Alphabet.a.rawValue
+        a.char = Alphabet.a.rawValue.uppercased()
         a.letterCase = Int16(LetterCase.upper.rawValue)
 
         let b = AlphabetEntity(context: context)
         b.uuid = UUID()
-        b.char = Alphabet.b.rawValue
+        b.char = Alphabet.b.rawValue.uppercased()
         b.letterCase = Int16(LetterCase.upper.rawValue)
 
         let c = AlphabetEntity(context: context)
         c.uuid = UUID()
-        c.char = Alphabet.c.rawValue
+        c.char = Alphabet.c.rawValue.uppercased()
         c.letterCase = Int16(LetterCase.upper.rawValue)
 
         let d = AlphabetEntity(context: context)
         d.uuid = UUID()
-        d.char = Alphabet.d.rawValue
+        d.char = Alphabet.d.rawValue.uppercased()
         d.letterCase = Int16(LetterCase.upper.rawValue)
 
         let e = AlphabetEntity(context: context)
         e.uuid = UUID()
-        e.char = Alphabet.e.rawValue
+        e.char = Alphabet.e.rawValue.uppercased()
         e.letterCase = Int16(LetterCase.upper.rawValue)
 
         let f = AlphabetEntity(context: context)
         f.uuid = UUID()
-        f.char = Alphabet.f.rawValue
+        f.char = Alphabet.f.rawValue.uppercased()
         f.letterCase = Int16(LetterCase.upper.rawValue)
 
         let g = AlphabetEntity(context: context)
         g.uuid = UUID()
-        g.char = Alphabet.g.rawValue
+        g.char = Alphabet.g.rawValue.uppercased()
         g.letterCase = Int16(LetterCase.upper.rawValue)
 
         let h = AlphabetEntity(context: context)
         h.uuid = UUID()
-        h.char = Alphabet.h.rawValue
+        h.char = Alphabet.h.rawValue.uppercased()
         h.letterCase = Int16(LetterCase.upper.rawValue)
 
         let i = AlphabetEntity(context: context)
         i.uuid = UUID()
-        i.char = Alphabet.i.rawValue
+        i.char = Alphabet.i.rawValue.uppercased()
         i.letterCase = Int16(LetterCase.upper.rawValue)
 
         let j = AlphabetEntity(context: context)
         j.uuid = UUID()
-        j.char = Alphabet.j.rawValue
+        j.char = Alphabet.j.rawValue.uppercased()
         j.letterCase = Int16(LetterCase.upper.rawValue)
 
         let k = AlphabetEntity(context: context)
         k.uuid = UUID()
-        k.char = Alphabet.k.rawValue
+        k.char = Alphabet.k.rawValue.uppercased()
         k.letterCase = Int16(LetterCase.upper.rawValue)
 
         let l = AlphabetEntity(context: context)
         l.uuid = UUID()
-        l.char = Alphabet.l.rawValue
+        l.char = Alphabet.l.rawValue.uppercased()
         l.letterCase = Int16(LetterCase.upper.rawValue)
 
         let m = AlphabetEntity(context: context)
         m.uuid = UUID()
-        m.char = Alphabet.m.rawValue
+        m.char = Alphabet.m.rawValue.uppercased()
         m.letterCase = Int16(LetterCase.upper.rawValue)
 
         let n = AlphabetEntity(context: context)
         n.uuid = UUID()
-        n.char = Alphabet.n.rawValue
+        n.char = Alphabet.n.rawValue.uppercased()
         n.letterCase = Int16(LetterCase.upper.rawValue)
 
         let o = AlphabetEntity(context: context)
         o.uuid = UUID()
-        o.char = Alphabet.o.rawValue
+        o.char = Alphabet.o.rawValue.uppercased()
         o.letterCase = Int16(LetterCase.upper.rawValue)
 
         let p = AlphabetEntity(context: context)
         p.uuid = UUID()
-        p.char = Alphabet.p.rawValue
+        p.char = Alphabet.p.rawValue.uppercased()
         p.letterCase = Int16(LetterCase.upper.rawValue)
 
         let q = AlphabetEntity(context: context)
         q.uuid = UUID()
-        q.char = Alphabet.q.rawValue
+        q.char = Alphabet.q.rawValue.uppercased()
         q.letterCase = Int16(LetterCase.upper.rawValue)
 
         let r = AlphabetEntity(context: context)
         r.uuid = UUID()
-        r.char = Alphabet.r.rawValue
+        r.char = Alphabet.r.rawValue.uppercased()
         r.letterCase = Int16(LetterCase.upper.rawValue)
 
         let s = AlphabetEntity(context: context)
         s.uuid = UUID()
-        s.char = Alphabet.s.rawValue
+        s.char = Alphabet.s.rawValue.uppercased()
         s.letterCase = Int16(LetterCase.upper.rawValue)
 
         let t = AlphabetEntity(context: context)
         t.uuid = UUID()
-        t.char = Alphabet.t.rawValue
+        t.char = Alphabet.t.rawValue.uppercased()
         t.letterCase = Int16(LetterCase.upper.rawValue)
 
         let u = AlphabetEntity(context: context)
         u.uuid = UUID()
-        u.char = Alphabet.u.rawValue
+        u.char = Alphabet.u.rawValue.uppercased()
         u.letterCase = Int16(LetterCase.upper.rawValue)
 
         let v = AlphabetEntity(context: context)
         v.uuid = UUID()
-        v.char = Alphabet.v.rawValue
+        v.char = Alphabet.v.rawValue.uppercased()
         v.letterCase = Int16(LetterCase.upper.rawValue)
 
         let w = AlphabetEntity(context: context)
         w.uuid = UUID()
-        w.char = Alphabet.w.rawValue
+        w.char = Alphabet.w.rawValue.uppercased()
         w.letterCase = Int16(LetterCase.upper.rawValue)
 
         let x = AlphabetEntity(context: context)
         x.uuid = UUID()
-        x.char = Alphabet.x.rawValue
+        x.char = Alphabet.x.rawValue.uppercased()
         x.letterCase = Int16(LetterCase.upper.rawValue)
 
         let y = AlphabetEntity(context: context)
         y.uuid = UUID()
-        y.char = Alphabet.y.rawValue
+        y.char = Alphabet.y.rawValue.uppercased()
         y.letterCase = Int16(LetterCase.upper.rawValue)
 
         let z = AlphabetEntity(context: context)
         z.uuid = UUID()
-        z.char = Alphabet.z.rawValue
+        z.char = Alphabet.z.rawValue.uppercased()
         z.letterCase = Int16(LetterCase.upper.rawValue)
     }
 
     private func initLowercaseAlphabet(context: NSManagedObjectContext) {
         let a = AlphabetEntity(context: context)
         a.uuid = UUID()
-        a.char = Alphabet.a.rawValue
+        a.char = Alphabet.a.rawValue.lowercased()
         a.letterCase = Int16(LetterCase.lower.rawValue)
 
         let b = AlphabetEntity(context: context)
         b.uuid = UUID()
-        b.char = Alphabet.b.rawValue
+        b.char = Alphabet.b.rawValue.lowercased()
         b.letterCase = Int16(LetterCase.lower.rawValue)
 
         let c = AlphabetEntity(context: context)
         c.uuid = UUID()
-        c.char = Alphabet.c.rawValue
+        c.char = Alphabet.c.rawValue.lowercased()
         c.letterCase = Int16(LetterCase.lower.rawValue)
 
         let d = AlphabetEntity(context: context)
         d.uuid = UUID()
-        d.char = Alphabet.d.rawValue
+        d.char = Alphabet.d.rawValue.lowercased()
         d.letterCase = Int16(LetterCase.lower.rawValue)
 
         let e = AlphabetEntity(context: context)
         e.uuid = UUID()
-        e.char = Alphabet.e.rawValue
+        e.char = Alphabet.e.rawValue.lowercased()
         e.letterCase = Int16(LetterCase.lower.rawValue)
 
         let f = AlphabetEntity(context: context)
         f.uuid = UUID()
-        f.char = Alphabet.f.rawValue
+        f.char = Alphabet.f.rawValue.lowercased()
         f.letterCase = Int16(LetterCase.lower.rawValue)
 
         let g = AlphabetEntity(context: context)
         g.uuid = UUID()
-        g.char = Alphabet.g.rawValue
+        g.char = Alphabet.g.rawValue.lowercased()
         g.letterCase = Int16(LetterCase.lower.rawValue)
 
         let h = AlphabetEntity(context: context)
         h.uuid = UUID()
-        h.char = Alphabet.h.rawValue
+        h.char = Alphabet.h.rawValue.lowercased()
         h.letterCase = Int16(LetterCase.lower.rawValue)
 
         let i = AlphabetEntity(context: context)
         i.uuid = UUID()
-        i.char = Alphabet.i.rawValue
+        i.char = Alphabet.i.rawValue.lowercased()
         i.letterCase = Int16(LetterCase.lower.rawValue)
 
         let j = AlphabetEntity(context: context)
         j.uuid = UUID()
-        j.char = Alphabet.j.rawValue
+        j.char = Alphabet.j.rawValue.lowercased()
         j.letterCase = Int16(LetterCase.lower.rawValue)
 
         let k = AlphabetEntity(context: context)
         k.uuid = UUID()
-        k.char = Alphabet.k.rawValue
+        k.char = Alphabet.k.rawValue.lowercased()
         k.letterCase = Int16(LetterCase.lower.rawValue)
 
         let l = AlphabetEntity(context: context)
         l.uuid = UUID()
-        l.char = Alphabet.l.rawValue
+        l.char = Alphabet.l.rawValue.lowercased()
         l.letterCase = Int16(LetterCase.lower.rawValue)
 
         let m = AlphabetEntity(context: context)
         m.uuid = UUID()
-        m.char = Alphabet.m.rawValue
+        m.char = Alphabet.m.rawValue.lowercased()
         m.letterCase = Int16(LetterCase.lower.rawValue)
 
         let n = AlphabetEntity(context: context)
         n.uuid = UUID()
-        n.char = Alphabet.n.rawValue
+        n.char = Alphabet.n.rawValue.lowercased()
         n.letterCase = Int16(LetterCase.lower.rawValue)
 
         let o = AlphabetEntity(context: context)
         o.uuid = UUID()
-        o.char = Alphabet.o.rawValue
+        o.char = Alphabet.o.rawValue.lowercased()
         o.letterCase = Int16(LetterCase.lower.rawValue)
 
         let p = AlphabetEntity(context: context)
         p.uuid = UUID()
-        p.char = Alphabet.p.rawValue
+        p.char = Alphabet.p.rawValue.lowercased()
         p.letterCase = Int16(LetterCase.lower.rawValue)
 
         let q = AlphabetEntity(context: context)
         q.uuid = UUID()
-        q.char = Alphabet.q.rawValue
+        q.char = Alphabet.q.rawValue.lowercased()
         q.letterCase = Int16(LetterCase.lower.rawValue)
 
         let r = AlphabetEntity(context: context)
         r.uuid = UUID()
-        r.char = Alphabet.r.rawValue
+        r.char = Alphabet.r.rawValue.lowercased()
         r.letterCase = Int16(LetterCase.lower.rawValue)
 
         let s = AlphabetEntity(context: context)
         s.uuid = UUID()
-        s.char = Alphabet.s.rawValue
+        s.char = Alphabet.s.rawValue.lowercased()
         s.letterCase = Int16(LetterCase.lower.rawValue)
 
         let t = AlphabetEntity(context: context)
         t.uuid = UUID()
-        t.char = Alphabet.t.rawValue
+        t.char = Alphabet.t.rawValue.lowercased()
         t.letterCase = Int16(LetterCase.lower.rawValue)
 
         let u = AlphabetEntity(context: context)
         u.uuid = UUID()
-        u.char = Alphabet.u.rawValue
+        u.char = Alphabet.u.rawValue.lowercased()
         u.letterCase = Int16(LetterCase.lower.rawValue)
 
         let v = AlphabetEntity(context: context)
         v.uuid = UUID()
-        v.char = Alphabet.v.rawValue
+        v.char = Alphabet.v.rawValue.lowercased()
         v.letterCase = Int16(LetterCase.lower.rawValue)
 
         let w = AlphabetEntity(context: context)
         w.uuid = UUID()
-        w.char = Alphabet.w.rawValue
+        w.char = Alphabet.w.rawValue.lowercased()
         w.letterCase = Int16(LetterCase.lower.rawValue)
 
         let x = AlphabetEntity(context: context)
         x.uuid = UUID()
-        x.char = Alphabet.x.rawValue
+        x.char = Alphabet.x.rawValue.lowercased()
         x.letterCase = Int16(LetterCase.lower.rawValue)
 
         let y = AlphabetEntity(context: context)
         y.uuid = UUID()
-        y.char = Alphabet.y.rawValue
+        y.char = Alphabet.y.rawValue.lowercased()
         y.letterCase = Int16(LetterCase.lower.rawValue)
 
         let z = AlphabetEntity(context: context)
         z.uuid = UUID()
-        z.char = Alphabet.z.rawValue
+        z.char = Alphabet.z.rawValue.lowercased()
         z.letterCase = Int16(LetterCase.lower.rawValue)
     }
 }
