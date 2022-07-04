4964dac0a6c9916485e86b751c823427390e7683
Tubagus Adhitya Permana <aditt7116@gmail.com>
2022-07-05 04:00:38 +0700

    add some assets

---
+++ LexPlay/Utils/UIScreen+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIScreen+Ext.swift
+//  LexPlay
+//
+//  Created by Tubagus Adhitya Permana on 05/07/22.
+//
+
+import UIKit
+
+extension UIScreen {
+  static let screenWidth = UIScreen.main.bounds.size.width
+  static let screenHeight = UIScreen.main.bounds.size.height
+}
