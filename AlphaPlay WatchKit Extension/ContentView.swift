96116a6564a04746ae5ef8e41b288e9c4761441e
Tubagus Adhitya Permana <aditt7116@gmail.com>
2022-07-19 15:10:59 +0700

    update apple watch app (alpha play)

---
+++ AlphaPlay WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlphaPlay WatchKit Extension
+//
+//  Created by Tubagus Adhitya Permana on 19/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AlphaPlay WatchKit Extension/LexPlayApp.swift
@@ -0,0 +1,21 @@
+//
+//  LexPlayApp.swift
+//  AlphaPlay WatchKit Extension
+//
+//  Created by Tubagus Adhitya Permana on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LexPlayApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
